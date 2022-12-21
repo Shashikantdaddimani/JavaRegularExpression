4307cab8505844cfe8cf5b6726442ee46ca3280e
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-12-21 16:47:16 +0530

    Added:Logic for Displaying the welcome meassage for regular expression program

---
+++ src/com/java/regex/RegularExpression.java
@@ -0,0 +1,10 @@
+package com.java.regex;
+
+public class RegularExpression {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome to the java regular expression program");
+
+	}
+
+}
