7e3cb05ba47018491c8a19ad149ae32db4b78dbd
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-12-21 20:49:02 +0530

    Added:Logic for Validating Last Name for using regular expression program

--- src/com/java/regex/RegularExpression.java
+++ src/com/java/regex/RegularExpression.java
@@ -1,4 +1,5 @@
 package com.java.regex;
+
 /**
  * imports all  the class of the java.util package
  */
@@ -10,9 +11,11 @@ public class RegularExpression {
 	/*
 	 * Logic for checking given regex is Valied or not
 	 */
-	static boolean  isValied(String regex, String input) {
+	private static boolean isValied(String regex, String input) {
+
 		return Pattern.compile(regex).matcher(input).matches();
 	}
+
 	/*
 	 * Program execution is start from main method
 	 */
@@ -20,18 +23,23 @@ public class RegularExpression {
 		/*
 		 * taking user input using scanner class
 		 */
-		Scanner sc = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		System.out.println("Welcome to the Java Regular Expressions programs");
 		/*
 		 * regular expression for FirstName
 		 */
 		String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
 		System.out.println("Enter the FirstName");
-		String firstName = sc.next();
-		
+		String firstName = input.next();
+		/*
+		 * Regular Expression for LastName
+		 */
+		String lastNamePattern = "^[A-Z]{1}[a-z]{2,}$";
+		System.out.println("Enter the LastName");
+		String lastName = input.next();
+
 		System.out.println(isValied(firstNamePattern, firstName));
 
 	}
-
 
 }
